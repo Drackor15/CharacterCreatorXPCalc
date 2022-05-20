dbfbcfcd468e62a6459efc5ef6085dce342cda69
Drackor15 <connor.knapp@trojans.dsu.edu>
2022-05-20 14:34:23 -0700

    Removed Unecessary Usings

--- EdArcCharacterCreatorXPCalc/Model/Character.cs
+++ EdArcCharacterCreatorXPCalc/Model/Character.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.Serialization;
-using System.Xml;
 using EdArcCharacterCreatorXPCalc.ModelInterfaces;
 
 namespace EdArcCharacterCreatorXPCalc.Model {
