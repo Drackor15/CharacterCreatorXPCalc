ce64bb575400332ae1f490166be7cb39c6f07c31
Drackor15 <connor.knapp@trojans.dsu.edu>
2022-07-16 13:06:48 -0700

    Implemented BindingProxy.cs so that child controls inherit parent datacontext. Delete button wired up.

---
+++ EdArcCharacterCreatorXPCalc/View/BindingProxy.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+/*
+ * Source Code Found Here: https://thomaslevesque.com/2011/03/21/wpf-how-to-bind-to-data-when-the-datacontext-is-not-inherited/
+ */
+
+namespace EdArcCharacterCreatorXPCalc.View {
+    public class BindingProxy : Freezable {
+
+        protected override Freezable CreateInstanceCore() {
+            return new BindingProxy();
+        }
+
+        public object Data {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+}
